f82d243fdd640c8699a4a6802cd3df1cebc3c51b
Tammarra McPhaul <shaemc22@gmail.com>
2021-01-27 20:19:13 -0500

    commit tree 925eda7b815e5453690105ba94479a91a6a41848

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,44 @@
-# code here!
+class School
+
+def initialize(name)
+    @name = name
+    @roster = {}
+end
+
+def name
+    @name
+end
+
+def roster
+    @roster
+end
+
+
+
+
+
+
+
+def add_student(student_name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student_name
+end
+
+def grade(grade)
+    @roster[grade]
+end
+
+def sort
+    hash = {}
+    @roster.keys.sort.each do |grade|
+    hash[grade] = @roster[grade].sort
+    end
+    hash
+end
+
+
+
+
+
+end
+
